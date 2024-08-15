9d094efa374ddaecf55a83f4dbf104297fcdcdcb
firedihm <42544757+firedihm@users.noreply.github.com>
2024-08-15 19:40:40 +0300

    Update formula.cpp

--- spreadsheet/formula.cpp
+++ spreadsheet/formula.cpp
@@ -1,1 +1,80 @@
-/* разместите здесь свой код */
+#include "formula.h"
+
+#include "FormulaAST.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <sstream>
+
+using namespace std::literals;
+
+FormulaError::FormulaError(Category category) 
+    : category_(category) {
+}
+
+FormulaError::Category FormulaError::GetCategory() const {
+    return category_;
+}
+
+bool FormulaError::operator==(FormulaError rhs) const {
+    return category_ == rhs.category_;
+}
+
+std::string_view FormulaError::ToString() const {
+    switch (category_) {
+        case Category::Ref:
+            return "#REF!";
+        case Category::Value:
+            return "#VALUE!";
+        case Category::Arithmetic:
+            return "#ARITHM!";
+        default:
+            return {};
+    }
+}
+
+std::ostream& operator<<(std::ostream& output, FormulaError fe) {
+    return output << fe.ToString();
+}
+
+namespace {
+class Formula : public FormulaInterface {
+public:
+    explicit Formula(std::string expression)
+        : ast_(ParseFormulaAST(std::move(expression))) {
+    }
+
+    Value Evaluate(const SheetInterface& sheet) const override {
+        try {
+            return ast_.Execute(sheet);
+        } catch (const FormulaError& e) {
+            return e;
+        }
+    }
+
+    std::string GetExpression() const override {
+        std::ostringstream out;
+        ast_.PrintFormula(out);
+        return out.str();
+    }
+
+    std::vector<Position> GetReferencedCells() const override {
+        std::forward_list<Position> cells_copy(ast_.GetCells());
+        cells_copy.sort();
+		cells_copy.unique();
+        return {std::move_iterator(cells_copy.begin()), std::move_iterator(cells_copy.end())};
+    }
+
+private:
+    FormulaAST ast_;
+};
+}  // namespace
+
+std::unique_ptr<FormulaInterface> ParseFormula(std::string expression) {
+    try {
+        return std::make_unique<Formula>(std::move(expression));
+    } catch (...) {
+        throw FormulaException("Invalid formula");
+    }
+}
